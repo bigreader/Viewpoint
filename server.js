8f3642df5643c5098ef6689a0ce598e19a4db66d
Joshua <bigreaderjd1996@gmail.com>
2019-06-25 15:56:27 -0500

    basic server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist").then(() => {
+  console.log('database connected');
+});
+
+app.listen(PORT, function () {
+  console.log('api server running on ' + PORT);
+});
